e0b0a5356d7b3bd3da1a50b1023c3456bbdc92e9
D-kit <d-kit@mail.ru>
2017-12-11 21:09:01 +0300

    Добавил мультимодальный maven-проект, spring-web-boot и начальную html-страницу.

---
+++ backend/web-boot/src/main/java/ru/dkit/real/logistic/WebApplication.java
@@ -0,0 +1,37 @@
+package ru.dkit.real.logistic;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Класс запуска приложения.
+ */
+@SpringBootApplication(scanBasePackages = {"ru.dkit.real.logistic"})
+public class WebApplication extends SpringBootServletInitializer {
+  @Value("${server.port:9000}")
+  private int port;
+
+  public static void main(String[] args) {
+    SpringApplication.run(WebApplication.class, args);
+  }
+
+  @Bean
+  public EmbeddedServletContainerFactory servletContainerFactory() {
+    TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory(port);
+    factory.setSessionTimeout(60, TimeUnit.MINUTES);
+    return factory;
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(WebApplication.class);
+  }
+}
---
+++ backend/web-boot/src/main/java/ru/dkit/real/logistic/configuration/WebMvcConfig.java
@@ -0,0 +1,11 @@
+package ru.dkit.real.logistic.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Конфигурация Spring MVC
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+}
