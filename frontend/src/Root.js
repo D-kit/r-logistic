57c831d690cefb86ac0f2f32c2c5fbd0f35616d8
D-kit <d-kit@mail.ru>
2017-12-24 11:56:02 +0300

    Добавил проекты frontend, model-api, model-impl, rest-api.

---
+++ frontend/src/Root.js
@@ -0,0 +1,14 @@
+// @flow
+
+import React from 'react';
+
+class Root extends React.Component {
+  render() {
+    return (
+        <div>
+          Real-Logistic
+        </div>
+    );
+  }
+}
+export default Root;
