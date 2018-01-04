adc174238161bfb1c6209532f6735d378ac2a3e0
D-kit <d-kit@mail.ru>
2018-01-05 01:22:32 +0300

    Настройка БД.

---
+++ backend/web-boot/src/main/java/ru/dkit/real/logistic/configuration/DataSourceConfig.java
@@ -0,0 +1,30 @@
+package ru.dkit.real.logistic.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+  @Value("${jdbc.driverClassName}")
+  private String driverClassName;
+  @Value("${jdbc.url}")
+  private String url;
+  @Value("${jdbc.username}")
+  private String username;
+  @Value("${jdbc.password}")
+  private String password;
+
+  @Bean
+  public DataSource dataSource() {
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    dataSource.setDriverClassName(driverClassName);
+    dataSource.setUrl(url);
+    dataSource.setUsername(username);
+    dataSource.setPassword(password);
+    return dataSource;
+  }
+}
---
+++ backend/web-boot/src/main/java/ru/dkit/real/logistic/configuration/JpaConfig.java
@@ -0,0 +1,71 @@
+package ru.dkit.real.logistic.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+public class JpaConfig {
+
+  private final DataSource dataSource;
+
+  @Value("${hibernate.dialect}")
+  private String hibernateDialect;
+  @Value("${hibernate.show_sql}")
+  private String hibernateShowSql;
+  @Value("${hibernate.hbm2ddl.auto}")
+  private String hibernateHbm2ddlAuto;
+  @Value("${hibernate.format_sql}")
+  private String hibernateFormatSql;
+
+  private String[] entityPackagesToScan = {"ru.dkit.real.logistic.**.entities"};
+
+  @Autowired
+  public JpaConfig(DataSource dataSource) {
+    this.dataSource = dataSource;
+  }
+
+  @Bean
+  public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+    LocalContainerEntityManagerFactoryBean lef = new LocalContainerEntityManagerFactoryBean();
+    lef.setDataSource(dataSource);
+    lef.setJpaVendorAdapter(jpaVendorAdapter());
+    lef.setPackagesToScan(entityPackagesToScan);
+    lef.setJpaProperties(getHibernateProperties());
+    return lef;
+  }
+
+  @Bean
+  public JpaVendorAdapter jpaVendorAdapter() {
+    HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
+    hibernateJpaVendorAdapter.setShowSql(true);
+    hibernateJpaVendorAdapter.setGenerateDdl(false);
+    hibernateJpaVendorAdapter.setDatabase(Database.HSQL);
+    return hibernateJpaVendorAdapter;
+  }
+
+  private Properties getHibernateProperties() {
+    Properties properties = new Properties();
+    properties.put("hibernate.dialect", hibernateDialect);
+    properties.put("hibernate.show_sql", hibernateShowSql);
+    properties.put("hibernate.hbm2ddl.auto", hibernateHbm2ddlAuto);
+    properties.put("hibernate.format_sql", hibernateFormatSql);
+    return properties;
+  }
+
+  @Bean
+  public JpaTransactionManager transactionManager() {
+    JpaTransactionManager transactionManager = new JpaTransactionManager();
+    transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+    return transactionManager;
+  }
+}
