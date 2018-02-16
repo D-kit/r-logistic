fd9565d4620ce796e0b41dc13180c24a018583ba
D-kit <d-kit@mail.ru>
2018-02-17 00:17:07 +0300

    Развитие frontend проекта.

---
+++ frontend/src/MainRoutes.js
@@ -0,0 +1,16 @@
+// @flow weak
+
+import React            from 'react';
+import {Route, Switch}  from 'react-router';
+import {About, Home}    from './pages';
+
+const MainRoutes = () => {
+  return (
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+      </Switch>
+  );
+};
+
+export default MainRoutes;
---
+++ frontend/src/config/index.js
@@ -0,0 +1,10 @@
+// @flow weak
+
+export default {
+  DEV_MODE: true, // flag to fetch mock or real fetch
+
+  api: {
+    host: 'http://localhost:9000/api',
+    fakeEndPoint: 'api/somewhere'
+  }
+};
