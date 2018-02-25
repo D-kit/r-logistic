83c7cbf4a456e8feda3e82fe26d3b5f4c7702039
D-kit <d-kit@mail.ru>
2018-02-26 00:07:50 +0300

    Настройка CORS для разработки.

--- backend/web-boot/src/main/java/ru/dkit/real/logistic/configuration/WebMvcConfig.java
+++ backend/web-boot/src/main/java/ru/dkit/real/logistic/configuration/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package ru.dkit.real.logistic.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -8,4 +9,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/api/**").allowedOrigins("http://localhost:3001").allowedMethods("*");
+  }
 }
